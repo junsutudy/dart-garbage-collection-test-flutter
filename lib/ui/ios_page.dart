b4d4e1f53eebc5b4ef591c4519b15709fd546a74
junsuPark <krpjs0508@gmail.com>
2024-08-28 15:50:33 +0900

    ios

---
+++ lib/ui/ios_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IosPage extends StatelessWidget {
+  const IosPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "images/ios_background",
+      width: double.infinity,
+      height: double.infinity,
+      fit: BoxFit.fitHeight,
+    );
+  }
+}
